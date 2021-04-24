24ebbcd39d35c97a7e003df9322fcf1a3812ecf7
adamkekesi <kekesiadi@gmail.com>
2021-04-24 16:04:55 +0200

    feature-player-data: PlayerData

---
+++ GTA6Game/PlayerData/PlayerSave.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTA6Game.PlayerData
+{
+    /// <summary>
+    /// The player's saved data
+    /// </summary>
+    public class PlayerSave : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event that gets invoked whenever a property of this object changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int money;
+
+        /// <summary>
+        /// The player's money
+        /// </summary>
+        public int Money
+        {
+            get { return money; }
+            set
+            {
+                money = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
