087e6927799b2b493e3f67ff699cd58c8a1e66dc
adamkekesi <kekesiadi@gmail.com>
2021-04-24 12:01:35 +0200

    feature-routing: PageBase
    The base class for all pages. Can use the router (RoutingHelper) to navigate to another page.

--- GTA6Game/Routing/PageBase.cs
+++ GTA6Game/Routing/PageBase.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using TeslaCarConfigurator.Data;
 
-namespace TeslaCarConfigurator.Helpers
+namespace GTA6Game.Routing
 {
     public class PageBase : Page
     {
         public RoutingHelper Router { get; set; }
-
-        public CarConfiguration Config { get; set; }
-
-        public MessageBarController MessageBarController { get; set; }
 
         public virtual void OnAttachedToFrame()
         {
