7ef89b76d50bd1b919f7e0b517dfe23944b5f2ea
adamkekesi <kekesiadi@gmail.com>
2021-05-02 23:04:16 +0200

    feature-multiple-profiles: Profile

--- GTA6Game/PlayerData/PlayerSave.cs
+++ GTA6Game/PlayerData/PlayerSave.cs
@@ -12,45 +12,18 @@ namespace GTA6Game.PlayerData
     /// The player's saved data
     /// </summary>
     [Serializable]
-    public class PlayerSave : INotifyPropertyChanged, IDisposable
+    public class PlayerSave : INotifyPropertyChanged
     {
-        /// <summary>
-        /// Creates a new save with default values
-        /// </summary>
-        /// <returns>The new save</returns>
-        public static PlayerSave CreateInitialSave()
-        {
-            return new PlayerSave() { Money = 1000 };
-        }
-
         /// <summary>
         /// Event that gets invoked whenever a property of this object changes
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private int money;
 
-        /// <summary>
-        /// The player's money
-        /// </summary>
-        public int Money
-        {
-            get { return money; }
-            set
-            {
-                money = value;
-                OnPropertyChanged();
-            }
-        }
 
         public PlayerSave()
         {
 
-        }
-
-        public void Dispose()
-        {
-            PropertyChanged = null;
         }
 
         private void OnPropertyChanged([CallerMemberName] string name = null)
